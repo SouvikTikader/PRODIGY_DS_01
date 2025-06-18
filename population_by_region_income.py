bebe434c3ae1e87a549518f516d714341db85ab2
SouvikTikader <142192788+SouvikTikader@users.noreply.github.com>
2025-06-18 15:33:43 +0530

    last

--- population_by_region_income.py
+++ population_by_region_income.py
@@ -4,11 +4,8 @@ import matplotlib.pyplot as plt
 # Load datasets
 df = pd.read_csv("dataset/API_SP.POP.TOTL_DS2_en_csv_v2_2590.csv", skiprows=4)
 meta = pd.read_csv("dataset/Metadata_Country_API_SP.POP.TOTL_DS2_en_csv_v2_2590.csv")
-
-# Merge on Country Code
 merged_df = df.merge(meta, on="Country Code")
 
-# Select year
 year = "2023"
 
 # Filter needed columns
